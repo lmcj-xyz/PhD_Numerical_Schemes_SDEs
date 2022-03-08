d3b6bc0732edb02c3dcbb0f1201c8283ccaf31a4
LM UoL laptop <mmlmcj@leeds.ac.uk>
2022-03-08 18:25:03 +0000

    name changed to sdedd.py in accordance to PEP 8 and change in coefficients to compute outside for loop

--- sdedd.py
+++ sdedd.py
@@ -5,6 +5,7 @@ Created on Mon Mar  7 19:05:37 2022
 @author: mmlmcj
 
 @title: Numerical Schemes for an SDE with distributional drift
+@note: sdedd stands for SDE with Distributional Drift
 """
 
 import numpy as np
@@ -50,8 +51,10 @@ class DistributionalSDE:
         self.h_hurst = h_hurst
         self.k_interval = k_interval
         self.m_time_steps = m_time_steps
+        self.fbm = self.create_fbm(self)
+        self.haar_coefficients = self.generate_haar_coefficients(self)
     
-    def fbm (self):
+    def create_fbm (self):
         """
         Generates a fractional Brownian motion
         (https://en.wikipedia.org/wiki/Fractional_Brownian_motion)
@@ -66,12 +69,39 @@ class DistributionalSDE:
 
         """
         points_fbm = 2**(self.n_haar+1)
-        x = np.linspace(start=1/points_fbm, stop=1., num=points_fbm, endpoint=True)
+        x = np.linspace(
+            start=1/points_fbm, 
+            stop=1.,
+            num=points_fbm,
+            endpoint=True
+            )
         g = rng.standard_normal(size=points_fbm)
         xg, yg = np.meshgrid(x, x, sparse=False, indexing='ij')
-        correlation_fbm = 0.5*(np.abs(xg)**(2*self.h_hurst) + np.abs(yg)**(2*self.h_hurst) - np.abs(xg - yg)**(2*self.h_hurst))
+        correlation_fbm = (
+            0.5*(np.abs(xg)**(2*self.h_hurst)
+                 + np.abs(yg)**(2*self.h_hurst)
+                 - np.abs(xg - yg)**(2*self.h_hurst))
+            )
         cholesky_fbm = np.linalg.cholesky(a=correlation_fbm)
         fbm_array = np.matmul(cholesky_fbm, g)
         fbm_array = np.insert(arr=fbm_array, obj=0, values=0)
         return fbm_array
+    
+    def generate_haar_coefficients(self):
+        j = np.arange(self.n_haar)
+        m = np.arange(2**(self.n_haar-1))
+        jg, mg = np.meshgrid(j, m, sparse=False, indexing='ij')
         
+        f = np.concatenate([self.fbm, np.zeros(2**(2*self.n_haar))])
+        
+        haar_coefficients = (-2**jg)*(
+            f[(mg+1)*2**(self.n_haar-jg)]
+            - 2*f[(2*mg+1)*2**(self.n_haar-1-jg)]
+            + f[mg*2**(self.n_haar-jg)]
+            )
+        
+        select = np.zeros((self.n_haar, 2**(self.n_haar-1)))
+        for k in range(self.n_haar):
+            for i in range(2**k):
+                select[k,i] = 1
+        return haar_coefficients*select
