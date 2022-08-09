5462df7fc1cda9334cc71f96db8d66de88e831d8
El LM <lmcj@lmcj.xyz>
2022-08-09 15:41:48 +0100

    Euler scheme has a problem with diffusion multiple of x or zero

--- euler.py
+++ euler.py
@@ -15,25 +15,134 @@ rng = default_rng()
 class Euler:
     """
     
-    This will give us the solution to an SDE that satisfies the usual assumptions, refer to Kloeden & Platen, Numerical Solutions for Stochastic Differential Equations
+    This will give us the solution to an SDE that satisfies the usual
+    assumptions, refer to Kloeden & Platen, Numerical Solutions for Stochastic
+    Differential Equations
 
     """
-    def __init__ (self, time_steps, time_start, time_end, drift, diffusion, y0):
+    def __init__ (self, drift, diffusion, time_steps = None, time_start = None, time_end = None, y0 = None, paths = None):
         """
 
         Parameters
         ----------
-        time_steps: int
-            The amount of time steps we use to compute the solution, which defines how fine is the grid.
-        time_start: float
-            The starting time of the scheme.
-        time_end: float
-            The ending time of the scheme.
         drift:
             Drift coefficient of the equation, satisfying usual conditions.
         diffusion:
             Diffusion coefficient of the equation, satisfying usual conditions.
-        y0: 
+        time_steps: int
+            The amount of time steps we use to compute the solution, 
+            which defines how fine is the grid.
+        time_start: float
+            The starting time of the scheme.
+        time_end: float
+            The ending time of the scheme.
+        y0: float
             Initial condition for the equation.
+        paths: int
+            Amount of paths to comput the solution.
+
+        Returns
+        -------
+        z: float
+            A random variable to use in the Euler scheme as the Brownian motion.
+        dt:
+            The step size in time.
+        time_grid: float
+            Time grid to compute the solution.
+        y: float
+            Placeholder for the solution of the SDE.
 
         """
+        self.drift = drift
+        self.diffusion = diffusion
+        self.time_steps = time_steps if time_steps is not None else 100
+        self.time_start = time_start if time_start is not None else 0
+        self.time_end = time_end if time_end is not None else 1
+        self.y0 = y0 if y0 is not None else 0
+        self.paths = paths if paths is not None else 100
+
+        """
+
+        Creation of the time grid
+
+        """
+        self.time_grid = np.linspace(
+                start = self.time_start, 
+                stop = self.time_end,
+                num = self.time_steps
+                )
+
+        """
+
+        Creation of normal random variable
+
+        """
+        self.dt = self.time_grid[1]
+        self.z = rng.normal(
+                loc = 0.0,
+                scale = np.sqrt(self.dt),
+                size = (self.paths, self.time_steps)
+                )
+
+        """
+
+        Creation of the placeholder for the solution.
+
+        """
+        self.y = np.zeros(shape = (self.paths, self.time_steps))
+        self.y[:, 0] = self.y0
+
+    """
+
+    Solve the SDE
+
+    """
+    def solve (self):
+        for i in range(self.time_steps - 1):
+            self.y[:, i+1] = self.y[:, i] \
+                    + self.drift(
+                            self.y[:, i], 
+                            self.time_grid[i]
+                            )*self.dt \
+                    + self.diffusion(
+                            self.y[:, i],
+                            self.time_grid[i]
+                            )*self.z[:, i+1]
+        return self.y
+
+    """
+
+    Plot the solutions
+
+    Here you can select the number of paths to plot with paths_plot
+
+    Remember that this method is not very useful in any other situation
+    besides testing
+
+    """
+    def plot_solution (self, paths_plot):
+        plt.figure()
+        plt.plot(self.y[range(paths_plot)].T)
+        plt.show()
+
+
+"""
+
+Testing area
+
+"""
+def mu(x, t):
+    return 1*x
+
+def sigma(x, t):
+    return x
+
+y = Euler(drift = mu, diffusion = sigma, time_steps = 100, paths = 10)#, paths_plot = 3)
+print("time steps:\n", y.time_steps)
+#print("time grid:", y.time_grid)
+#print("dt:\n", y.dt)
+print("random variable:\n", y.z)
+#print("solution y placeholder:\n", y.y)
+#print("drift:\n", y.drift(y.y, y.time_grid))
+print("solution:\n", y.solve())
+y.plot_solution(paths_plot = 3)
