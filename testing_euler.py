a5f6ff8c2fdc0a7c40e61ba8386293700f348299
El LM <lmcj@lmcj.xyz>
2022-08-11 19:17:10 +0100

    Just indentation changes, i think

--- testing_euler.py
+++ testing_euler.py
@@ -20,32 +20,29 @@ y = e.Euler(drift = mu, diffusion = sigma, time_steps = 10, paths = 10)
 #print("random variable:\n", y.z)
 #print("solution y placeholder:\n", y.y)
 #print("drift:\n", y.drift(y.y, y.time_grid))
-print("solution:\n", y.solve())
+#print("solution:\n", y.solve())
 #y.plot_solution(paths_plot = 5, save_plot = True)
-y.plot_solution(paths_plot = 5, save_plot = False)
+#y.plot_solution(paths_plot = 5, save_plot = False)
 
 # Creation of an explicit GBM to compare
 ########## This is wrong, check
 gbm = np.zeros(shape = (y.paths, y.time_steps))
 gbm[:, 0] = y.y0
+print(gbm)
 
 for i in range(y.time_steps - 1):
-    gbm[:, i+1] = y.y0*np.exp( 
-            ( 
+    gbm[:, i+1] = y.y0*np.exp((
                 mu(gbm[:, i], y.time_grid[i]) 
                 - 
-                sigma(gbm[:, i], y.time_grid[i])/2 
-                )*y.dt
-            + sigma(
-                gbm[:, i], y.time_grid[i])*y.z[:, i].T
-            )
+                sigma(gbm[:, i], y.time_grid[i])/2)*y.dt
+            + sigma(gbm[:, i], y.time_grid[i])*y.z[:, i].T)
 
-print(gbm)
-plt.figure()
-plt.plot(gbm[0:4, :].T)
-plt.show()
-
-dif = gbm - y.solve()
-print(dif)
-
-print(np.amax(abs(dif), axis=1))
+#print(gbm)
+#plt.figure()
+#plt.plot(gbm[0:4, :].T)
+#plt.show()
+#
+#dif = gbm - y.solve()
+#print(dif)
+#
+#print(np.amax(abs(dif), axis=1))
