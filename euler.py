76bc28a47e8847a2e74a7e54edb8bb1947cb7c5e
El LM <lmcj@lmcj.xyz>
2022-08-16 18:48:29 +0100

    grid in a method, let's see how this continues

--- euler.py
+++ euler.py
@@ -67,44 +67,100 @@ class Euler:
         # Initialize arguments
         self.drift = drift
         self.diffusion = diffusion
-        self.time_steps = time_steps if time_steps is not None else 100
-        self.time_start = time_start if time_start is not None else 0
-        self.time_end = time_end if time_end is not None else 1
-        self.y0 = y0 if y0 is not None else 1
-        self.paths = paths if paths is not None else 100
+        self.time_steps = time_steps if time_steps \
+                is not None else 10**2
+        self.time_start = time_start if time_start \
+                is not None else 0
+        self.time_end = time_end if time_end \
+                is not None else 1
+        self.y0 = y0 if y0 \
+                is not None else 1
+        self.paths = paths if paths \
+                is not None else 100
 
-        # Creation of the time grid
-        self.time_grid = np.linspace(
-                start = self.time_start, 
-                stop = self.time_end,
-                num = self.time_steps
-                )
+        # Time step
+        self.dt = (self.time_end - self.time_start) / self.time_steps
 
         # Creation of normal random variable
-        self.dt = self.time_grid[1]
+        ## Since the time grid would start in t_1, and the random variable
+        ## in z_1 we have a bijection between the time grid and each path
+        ## of the rv which cannot achieved if we start the time grid in t_0.
+        ## Well it can be achieved, but is not what we need given the recursive
+        ## nature of the Brownian motion when having coarse grids
         self.z = rng.normal(
                 loc = 0.0,
                 scale = np.sqrt(self.dt),
                 size = (self.paths, self.time_steps)
                 )
+        # Creation of the time grid
+        ## We create the time grid starting from the first non-zero term,
+        ## effectively we do not need to use the first time (typically 0)
+        ## in any moment
+        ## Also this is the finest grid, in the solve method we can use this
+        ## or a coarser one
+        self.time_grid = self.generate_time_grid()
 
-    def solve (self):
+    #def coarse_z (self, self.z):
+
+    #def coarse_time_grid (self, self.time_grid):
+
+    #def rate (self):
+
+    def generate_time_grid (
+            self, 
+            time_start_grid = None,
+            time_end_grid = None,
+            dt_grid = None,
+            time_steps_grid = None
+            ):
+        time_start_grid = time_start_grid if time_start_grid \
+                is not None else self.time_start
+        time_end_grid = time_end_grid if time_end_grid \
+                is not None else self.time_end
+        time_steps_grid = time_steps_grid if time_steps_grid \
+                is not None else self.time_steps
+        dt_grid = dt_grid if dt_grid \
+                is not None else self.dt
+
+        time_grid_generated = np.linspace(
+                start = time_start_grid + dt_grid, 
+                stop = time_end_grid,
+                num = time_steps_grid
+                )
+        return time_grid_generated
+
+    def solve (
+            self,
+            time_steps_solve = None,
+            #time_grid_solve = None,
+            dt_solve = None,
+            z_solve = None
+            ):
+        time_steps_solve = time_steps_solve if time_steps_solve \
+                is not None else self.time_steps
+        time_grid_solve = self.generate_time_grid(time_steps_solve) if time_steps_solve \
+                is not None else self.time_grid
+        dt_solve = dt_solve if dt_solve \
+                is not None else self.dt
+        z_solve = z_solve if z_solve \
+                is not None else self.z
+
         """ Solve the SDE """
         # Creation of the placeholder for the solution.
-        self.y = np.zeros(shape = (self.paths, self.time_steps))
+        self.y = np.zeros(shape = (self.paths, time_steps_solve))
         # And adding intial condition.
         self.y[:, 0] = self.y0
 
-        for i in range(self.time_steps - 1):
+        for i in range(time_steps_solve - 1):
             self.y[:, i+1] = self.y[:, i] \
                     + self.drift(
                             self.y[:, i], 
-                            self.time_grid[i]
-                            )*self.dt \
+                            time_grid_solve[i]
+                            )*dt_solve \
                     + self.diffusion(
                             self.y[:, i],
-                            self.time_grid[i]
-                            )*self.z[:, i+1]
+                            time_grid_solve[i]
+                            )*z_solve[:, i+1]
         return self.y
 
     def plot_solution (self, paths_plot, save_plot = False):
--- testing_euler.py
+++ testing_euler.py
@@ -11,16 +11,22 @@ def sigma(x, t):
 # Option to print only 3 decimal places
 np.set_printoptions(precision = 3)
 
-y = e.Euler(drift = mu, diffusion = sigma, time_steps = 10, paths = 10)
+y = e.Euler(
+        drift = mu,
+        diffusion = sigma,
+        time_steps = 10**1,
+        #time_end = 10,
+        paths = 10
+        )
 #, paths_plot = 3)
 
 #print("time steps:\n", y.time_steps)
-#print("time grid:", y.time_grid)
+print("time grid:", y.time_grid)
 #print("dt:\n", y.dt)
 #print("random variable:\n", y.z)
 #print("solution y placeholder:\n", y.y)
 #print("drift:\n", y.drift(y.y, y.time_grid))
-print("solution:\n", y.solve())
+#print("solution:\n", y.solve())
 #y.plot_solution(paths_plot = 5, save_plot = True)
 y.plot_solution(paths_plot = 5, save_plot = False)
 
@@ -31,19 +37,25 @@ gbm[:, 0] = 1
 #print(gbm)
 
 for i in range(y.time_steps - 1):
-    gbm[:, i+1] = gbm[:, i]*np.exp((
-                mu(gbm[:, i], y.time_grid[i]) 
-                - 
-                sigma(gbm[:, i], y.time_grid[i])**2 / 2)*y.time_grid[i]
-            + sigma(gbm[:, i], y.time_grid[i])*y.z[:, i+1])
+    gbm[:, i+1] = gbm[:, 0] * np.exp(
+                ( 
+                    #mu(gbm[:, i], y.time_grid[i])
+                    1
+                    - 
+                    #sigma(gbm[:, i], y.time_grid[i])**2 / 2
+                    0.05**2 / 2
+                )*y.time_grid[i]
+            #+ sigma(gbm[:, i], y.time_grid[i])*y.z[:, i+1]
+            + 0.05*y.z[:, i+1]
+            )
 
 #gbm*y.y0
 
-print(gbm)
+#print(gbm)
 plt.figure()
 plt.plot(gbm[0:4, :].T)
 plt.show()
-#
+
 #dif = gbm - y.solve()
 #print(dif)
 #
