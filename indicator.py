e2f9d1c6d7ee2966dd7e95e12437d6ece3607e94
El LM <lmcj@lmcj.xyz>
2022-08-24 18:53:46 +0100

    boilerplate indicator.py

--- indicator.py
+++ indicator.py
@@ -1,3 +1,26 @@
 import Euler as e
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Time steps
+m = 10**6
+
+# Distributional drift
+def bn (t, x):
+    return 0
+
+# Constant diffusion
+def sigma (t, x):
+    return 0
+
+# Euler approximation
+y = e.Euler(
+        drift = bn,
+        diffusion = sigma,
+        time_steps = m,
+        paths = 10,
+        y0 = 0
+        )
+
+# Rate of convergence
+print(y.rate())
