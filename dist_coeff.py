4aa37d0e98d7e0016072ef4dae08db25529360fd
El LM <lmcj@lmcj.xyz>
2022-09-12 20:26:52 +0100

    some implementation of the gaussian differences

--- dist_coeff.py
+++ dist_coeff.py
@@ -47,6 +47,10 @@ class distribution:
                 )
 
         self.fbm_path = self.fbm()
+
+        self.normal_differences = np.zeros_like(self.grid)
+        for i in self.grid:
+            self.normal_differences[np.where(self.grid = i)] = self.normal_differences(i, 1, 10)
     
     def fbm(self):
         x_grid, y_grid = np.meshgrid(
@@ -65,19 +69,37 @@ class distribution:
         fbm_array = np.matmul(cholesky, g)
         return fbm_array
 
-    def b(self, x, t, m):
+    def normal_difference(self, x, t, m):
         diff_norm = np.zeros_like(self.grid)
         length_grid = np.shape(self.grid)[0]
-        dx = 2*self.limit/length_grid
+        #print("length of grid: ", length_grid)
+        #dx = 2*self.limit/length_grid
+        dx = self.limit/length_grid
         for i in range(length_grid):
-            diff_norm[i] = norm(i + dx, loc=i, scale=1/(m**(8/3))) \
-                    - norm(i - dx, loc=i, scale=1/(m**(8/3)))
-        b_array = diff_norm
-        return b_array
+            #print("element: ", self.grid[i])
+            #print("element + dx: ", self.grid[i]+dx)
+            #print("element - dx: ", self.grid[i]-dx)
+            diff_norm[i] = \
+                    norm.cdf(
+                    self.grid[i] + dx, 
+                    #loc=self.grid[i], 
+                    loc=x, 
+                    scale=1/(m**(8/3))
+                    ) -\
+                    norm.cdf(
+                    self.grid[i] - dx, 
+                    #loc=self.grid[i], 
+                    loc=x, 
+                    scale=1/(m**(8/3))
+                    )
+        diff_norm
+        return diff_norm
+
+    
         
        
 # Tests
-x = distribution(hurst = 0.75, limit = 4, points = 10)
+x = distribution(hurst = 0.75, limit = 4, points = 10**3)
 #print(x.grid)
 ## Covariance matrix
 #cov = x.fbm()
@@ -90,4 +112,8 @@ x = distribution(hurst = 0.75, limit = 4, points = 10)
 #plt.figure()
 #plt.plot(x.grid, frac)
 #plt.show()
-print(x.b(x=2, t=1, m=10))
+#print("grid: ", x.grid)
+print("value of b: ", x.b(x=1, t=1, m=10))
+plt.figure()
+plt.plot(x.grid, x.b(x=3, t=1, m=10))
+plt.show()
