d7f8a44b11ed4dd126e61cc1796df34f2649f55f
LM UoL laptop <mmlmcj@leeds.ac.uk>
2022-12-13 06:29:22 +0000

    some examples of measurable drifts

--- sym_comp.py
+++ sym_comp.py
@@ -4,9 +4,10 @@ Created on Wed Nov 30 15:40:52 2022
 
 @author: mmlmcj
 """
-
+#%%
 from sympy import symbols, exp, sqrt, pi, limit, oo, plot
 
+#%%
 z = symbols('z')
 
 expr = z * exp(-z**2/2) / sqrt(4*pi)
@@ -16,3 +17,24 @@ limit_expr = limit(expr, z, oo)
 print(limit_expr)
 
 plot(expr, xlim=[-10, 10])
+
+#%%
+from sympy import Abs
+
+#%%
+x = symbols('x')
+epsilon = 0.00001
+beta1 = epsilon
+beta2 = 1/128
+beta3 = 1/64
+beta4 = 1/32
+beta5 = 1/16
+beta6 = 1/8
+beta7 = 1/4 - epsilon
+
+mfunct1 = - Abs(x)**(1 - beta1)
+mfunct2 = - Abs(x)**(1 - beta2)
+
+plot(mfunct1, xlim=[-10, 10])
+plot(mfunct2, xlim=[-10, 10])
+
