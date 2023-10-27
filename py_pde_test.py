9637903a7fdfa0acf367f2e5535f14386bd4d5b5
LMCJ T470 <dev@lmcj.xyz>
2023-10-27 09:31:52 +0100

    Rename of py-pde tests

--- py_pde_test.py
+++
@@ -1,8 +0,0 @@
-from pde import CartesianGrid, ScalarField, PDE, MemoryStorage
-
-grid = CartesianGrid([[0, 10], [0, 5]], [20, 10], periodic=[True, False])
-field = ScalarField(grid)
-eq = PDE({"u": "-gradient_squared(u) / 2 - laplace(u - laplace(u))"})
-storage = MemoryStorage()
-result = eq.solve(field, t_range=10, dt=1e-2, tracker=["progress", storage.tracker(1)])
-result.plot()
