38ce21d9ea6deb0aa27ba6617c6c3b69f9863376
LM T410 <dev@lmcj.xyz>
2024-07-30 11:51:01 +0100

    Debugging solve_fp

--- test_mv2.py
+++ test_mv2.py
@@ -2,6 +2,8 @@ import dsdes as ds
 from numpy.random import default_rng
 import numpy as np
 import sys
+from scipy.stats import norm
+import pde
 
 
 rng = default_rng()
@@ -42,7 +44,27 @@ integral_grid = ds.integral_between_grid_points(var_heat_kernel, grid_x, half_su
 # Drift creation
 drift_array = ds.create_drift_array(bridge_array, integral_grid),
 
-ds.solve_fp(drift_array, grid_x, half_support, lambda x: np.cos(x), ts=0, te=1)
+#ds.solve_fp(drift_array, grid_x, half_support, lambda x: np.cos(x), ts=0, te=1)
+xn = 2**3
+x = np.linspace(norm.ppf(0.01), norm.ppf(0.99), xn)
+ic = norm.pdf(x)
+grid_bounds = (-half_support, half_support)
+grid = pde.CartesianGrid(bounds=[grid_bounds], shape=xn, periodic=False)
+state = pde.ScalarField(grid=grid, data=ic)
+storage = pde.MemoryStorage()
+
+
+def drift_f(x: np.ndarray, drift_array=drift_array, grid=grid_x):
+    return np.interp(x=x.data, xp=grid, fp=drift_array)
+
+
+eq = ds.FokkerPlanckPDE(drift=drift_f, nonlinear=lambda x: np.sin(x))
+solver = pde.ScipySolver(pde=eq)
+time_steps = 10
+dt = 1/time_steps
+time_range = (time_start, time_end)
+cont = pde.Controller(solver=solver, t_range=time_range, tracker=storage.tracker(dt))
+soln = cont.run(state)
 
 
 #if __name__ == "__main__":
