8cf2a082975cc823422a481eafc4d6932e8f5efd
LMCJ T470 <dev@lmcj.xyz>
2024-09-06 15:54:14 +0100

    Batches/error scripts done

--- playground/error.py
+++ playground/error.py
@@ -1,54 +1,51 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
 from scipy.stats import linregress
 import pickle
+import time
 
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import dsdes as ds
 
-rng = np.random.default_rng(seed=1392917848)
+# Graphical parameters
+params = {
+   'axes.labelsize': 8,
+   'font.size': 8,
+   'legend.fontsize': 10,
+   'xtick.labelsize': 10,
+   'ytick.labelsize': 10,
+   'text.usetex': False,
+   'figure.figsize': [4.5, 4.5]
+   }
+rcParams.update(params)
 
-plt.rcParams['figure.dpi'] = 200
+lgreen = (0, 0.31, 0.18)
+lred = (0.77, 0.07, 0.19)
+lwhite = (0.96, 0.95, 0.89)
+
+# Code
+rng = np.random.default_rng(seed=1392917848)
 
 # Parameters for Euler scheme
 keys = ('real', 'approx1', 'approx2', 'approx3', 'approx4', 'approx5')
 
-time_steps_tuple = (2**15, 2**8, 2**9, 2**10, 2**11, 2**12)
+time_steps_tuple = (2**13, 2**6, 2**7, 2**8, 2**9, 2**10)
 time_steps = dict(zip(keys, time_steps_tuple))
 
 error_keys = ('e1', 'e2', 'e3', 'e4', 'e5')
 
 epsilon = 10e-6
-beta = 1/2 - epsilon
+beta = epsilon
 hurst = 1 - beta
-y0 = 1
 sample_paths = 10**4
+y0 = rng.normal(size=sample_paths)
 time_start = 0
 time_end = 1
-dt_tuple = tuple(
-        map(
-            lambda t: (time_end - time_start)/(t - 1), time_steps.values()
-            )
-        )
-dt = dict(zip(keys, dt_tuple))
-time_grid_tuple = tuple(
-        map(
-            lambda dt, t: np.linspace(time_start + dt, time_end, t),
-            dt.values(),
-            time_steps.values()
-            )
-        )
-time_grid = dict(zip(keys, time_grid_tuple))
-
-noise = rng.normal(
-        loc=0.0, scale=np.sqrt(dt['real']),
-        size=(time_steps['real'], sample_paths)
-        )
-
 # Parameters to create fBm
-points_x = 2**12  # According to the lower bound in the paper
+points_x = 2**10  # According to the lower bound in the paper
 half_support = 10
 
 eta = 1/((hurst-1/2)**2 + 2 - hurst)
@@ -64,74 +61,95 @@ delta_x = half_support/(points_x-1)
 grid_x = np.linspace(start=-half_support, stop=half_support, num=points_x)
 grid_x0 = np.linspace(start=0, stop=2*half_support, num=points_x)
 gaussian_fbm = rng.standard_normal(size=points_x)
-drift_tuple = tuple(
-        map(
-            lambda t: ds.drift(gaussian_fbm, hurst, points_x, half_support, t)[0],
-            time_steps.values()
-            )
-        )
+
+drift_tuple = tuple(map(
+    lambda t: ds.drift(gaussian_fbm, hurst, points_x, half_support, t)[0],
+    time_steps.values()
+    ))
 drift_array = dict(zip(keys, drift_tuple))
+# plt.plot(drift_array['real'])
+# plt.show()
 
-solution_tuple = tuple(
-        map(
-            lambda d, t: ds.solve(
-                y0, d, noise, time_start, time_end, t, sample_paths, grid_x
-                ),
-            drift_array.values(),
-            time_steps.values(),
-            )
+dt_tuple = tuple(
+        map(lambda t: (time_end - time_start)/(t - 1), time_steps.values())
         )
-solution = dict(zip(keys, solution_tuple))
+dt = dict(zip(keys, dt_tuple))
+time_grid_tuple = tuple(map(
+    lambda dt, t: np.linspace(time_start + dt, time_end, t),
+    dt.values(), time_steps.values()
+    ))
+time_grid = dict(zip(keys, time_grid_tuple))
 
-strong_error = dict.fromkeys(error_keys)
-strong_error['e1'] = np.abs(solution['real'] - solution['approx1'])
-strong_error['e2'] = np.abs(solution['real'] - solution['approx2'])
-strong_error['e3'] = np.abs(solution['real'] - solution['approx3'])
-strong_error['e4'] = np.abs(solution['real'] - solution['approx4'])
-strong_error['e5'] = np.abs(solution['real'] - solution['approx5'])
+loopint = 40
+for i in range(loopint):
+    # loop here?
+    noise = rng.normal(loc=0.0, scale=np.sqrt(dt['real']),
+                       size=(time_steps['real'], sample_paths))
 
-plot_error = [np.mean(value) for key, value in strong_error.items()]
-plot_dt = [value for key, value in dt.items() if key not in 'real']
+    solution_tuple = tuple(map(
+        lambda d, t: ds.solve(
+            y0, d, noise, time_start, time_end, t, sample_paths, grid_x
+            ),
+        drift_array.values(),
+        time_steps.values(),
+        ))
+    solution = dict(zip(keys, solution_tuple))
 
-log_strong_error = np.log10(plot_error)
-log_dt = np.log10(plot_dt)
+    strong_error = dict.fromkeys(error_keys)
+    strong_error['e1'] = np.abs(solution['real'] - solution['approx1'])
+    strong_error['e2'] = np.abs(solution['real'] - solution['approx2'])
+    strong_error['e3'] = np.abs(solution['real'] - solution['approx3'])
+    strong_error['e4'] = np.abs(solution['real'] - solution['approx4'])
+    strong_error['e5'] = np.abs(solution['real'] - solution['approx5'])
 
-reg_strong = linregress(log_dt, log_strong_error)
-rate_strong = reg_strong.slope
-intersection_strong = reg_strong.intercept
+    plot_error = [np.mean(value) for key, value in strong_error.items()]
+    plot_dt = [value for key, value in dt.items() if key not in 'real']
 
-plot_dict = {
-        'beta': beta,
-        'rate': rate_strong,
-        'dt': plot_dt,
-        'error': plot_error,
-        }
+    log_strong_error = np.log10(plot_error)
+    log_dt = np.log10(plot_dt)
 
-lgreen = (0, 0.31, 0.18)
-lred = (0.77, 0.07, 0.19)
-lwhite = (0.96, 0.95, 0.89)
+    reg_strong = linregress(log_dt, log_strong_error)
+    rate_strong = reg_strong.slope
+    intersection_strong = reg_strong.intercept
 
-fig, ax = plt.subplots()
-ax.set_title(
-    r'Rate of convergence r = %f for $\beta$=%f' % (rate_strong, beta)
-    )
-ax.plot(plot_dt,
-        plot_error,
-        marker='o',
-        label='Strong error',
-        color=lgreen)
-ax.grid(which='both', linestyle='--')
-ax.set_yscale('log')
-ax.set_xscale('log')
-ax.set_xlabel(r'$\log_{10}(\Delta t)$')
-ax.set_ylabel(r'$\log_{10}(\epsilon)$')
-ax.legend()
-plt.show()
+    plot_dict = {
+            'beta': beta,
+            'rate': rate_strong,
+            'dt': plot_dt,
+            'error': plot_error,
+            }
 
-saving = 'no'
-#saving = input('Do you want to save to files the plot and its corresponding dictionary? (yes/no): ')
-if saving == 'yes':
-    fig.savefig('rate.pdf', dpi=200)
-    with open('dict_plot.pkl', 'wb') as fp:
-        pickle.dump(plot_dict, fp)
-        print('Files saved succesfully')
+    plot = False
+    if plot:
+        fig, ax = plt.subplots()
+        ax.set_title(
+                r'Rate of convergence r = %f for $\beta$=%f' % (rate_strong, beta)
+                )
+        ax.plot(plot_dt,
+                plot_error,
+                marker='o',
+                label='Strong error',
+                color=lgreen)
+        ax.grid(which='both', linestyle='--')
+        ax.set_yscale('log')
+        ax.set_xscale('log')
+        ax.set_xlabel(r'$\log_{10}(\Delta t)$')
+        ax.set_ylabel(r'$\log_{10}(\epsilon)$')
+        ax.legend()
+        plt.show()
+
+    #saving = input('Do you want to save to files the plot and its corresponding dictionary? (yes/no): ')
+    saving = False
+    if saving:
+        date_string = time.strftime("%Y-%m-%d-%H-%M")
+        fig.savefig(date_string + 'rate.pdf', dpi=200)
+        with open(date_string + 'dict_plot.pkl', 'wb') as fp:
+            pickle.dump(plot_dict, fp)
+            print('Files saved succesfully')
+
+    if loopint == 40:
+        print('rate = ', rate_strong)
+        file = open("rates_linear.txt", "a")
+        file.write(str(rate_strong))
+        file.write("\n")
+        file.close()
--- playground/error_mv.py
+++ playground/error_mv.py
@@ -1,6 +1,6 @@
 import numpy as np
-from numpy.random import default_rng
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
 from scipy.stats import linregress
 import pickle
 import time
@@ -10,12 +10,24 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import dsdes as ds
 
+# Graphical parameters
+params = {
+   'axes.labelsize': 8,
+   'font.size': 8,
+   'legend.fontsize': 10,
+   'xtick.labelsize': 10,
+   'ytick.labelsize': 10,
+   'text.usetex': False,
+   'figure.figsize': [4.5, 4.5]
+   }
+rcParams.update(params)
 
-start_time = time.time()
+lgreen = (0, 0.31, 0.18)
+lred = (0.77, 0.07, 0.19)
+lwhite = (0.96, 0.95, 0.89)
 
-rng = default_rng()
-
-#plt.rcParams['figure.dpi'] = 200
+# Code
+rng = np.random.default_rng(seed=1392917848)
 
 # Parameters for Euler scheme
 keys = ('real', 'approx1', 'approx2', 'approx3', 'approx4', 'approx5')
@@ -26,10 +38,9 @@ time_steps = dict(zip(keys, time_steps_tuple))
 error_keys = ('e1', 'e2', 'e3', 'e4', 'e5')
 
 epsilon = 10e-6
-beta = 1/2
+beta = 1/2 - epsilon
 hurst = 1 - beta
 sample_paths = 10**4
-#y0 = np.ones_like(sample_paths)
 y0 = rng.normal(size=sample_paths)
 time_start = 0
 time_end = 1
@@ -50,158 +61,128 @@ delta_x = half_support/(points_x-1)
 grid_x = np.linspace(start=-half_support, stop=half_support, num=points_x)
 grid_x0 = np.linspace(start=0, stop=2*half_support, num=points_x)
 gaussian_fbm = rng.standard_normal(size=points_x)
-fbm_array = ds.fbm(gaussian_fbm, hurst, points_x, half_support)
-bridge_array = ds.bridge(fbm_array, grid_x0)
+
+drift_tuple = tuple(map(
+    lambda t: ds.drift(gaussian_fbm, hurst, points_x, half_support, t)[0],
+    time_steps.values()
+    ))
+drift_array = dict(zip(keys, drift_tuple))
+# plt.plot(drift_array['real'])
+# plt.show()
 
 dt_tuple = tuple(
-        map(
-            lambda t: (time_end - time_start)/(t - 1), time_steps.values()
-            )
+        map(lambda t: (time_end - time_start)/(t - 1), time_steps.values())
         )
 dt = dict(zip(keys, dt_tuple))
-time_grid_tuple = tuple(
-        map(
-            lambda d, t: np.linspace(time_start + d, time_end, t),
-            dt.values(),
-            time_steps.values()
-            )
-        )
+time_grid_tuple = tuple(map(
+    lambda d, t: np.linspace(time_start + d, time_end, t),
+    dt.values(), time_steps.values()
+    ))
 time_grid = dict(zip(keys, time_grid_tuple))
 
-noise = rng.normal(
-        loc=0.0, scale=np.sqrt(dt['real']),
-        size=(time_steps['real'], sample_paths)
-        )
 
-# Variance of heat kernel
-var_tuple = tuple(map(lambda t: ds.heat_kernel_var(t, hurst), time_steps.values()))
-var_heat_kernel = dict(zip(keys, var_tuple))
-
-# Integral between grid points
-integral_tuple = tuple(
-        map(
-            lambda t: ds.integral_between_grid_points(t, grid_x, half_support),
-            var_heat_kernel.values()
-            )
-        )
-integral_grid = dict(zip(keys, integral_tuple))
-
-# Drift creation
-drift_tuple = tuple(
-        map(
-            lambda i: ds.create_drift_array(bridge_array, i),
-            integral_grid.values()
-            )
-        )
-drift_array = dict(zip(keys, drift_tuple))
-
-
+# Nonlinear function
 def nonl(x):
     return np.sin(x)
-    #return x**0
 
 
-#pde_solution_tuple = tuple(
-#        map(
-#            lambda d: ds.solve_fp(),
-#            drift_array.values()
-#            )
-#        )
+points_t = 2**5  # for the FP approx
 
+loopint = 40
+for i in range(loopint):
+    # loop here?
+    noise = rng.normal(loc=0.0, scale=np.sqrt(dt['real']),
+                       size=(time_steps['real'], sample_paths))
+    solution_tuple = tuple(map(
+        lambda d, t: ds.solve_mv(
+            y0, d, noise,
+            time_start, time_end, t,
+            sample_paths, grid_x, half_support, points_x, points_t,
+            nonl
+            ),
+        drift_array.values(),
+        time_steps.values(),
+        ))
+    solution = dict(zip(keys, solution_tuple))
 
-points_t = 2**5
-solution_tuple = tuple(
-        map(
-            lambda d, t: ds.solve_mv(
-                y0, d, noise,
-                time_start, time_end, t,
-                sample_paths, grid_x, half_support, points_x, points_t,
-                nonl
-                ),
-            drift_array.values(),
-            time_steps.values(),
+    strong_error = dict.fromkeys(error_keys)
+    strong_error['e1'] = np.abs(solution['real'][0] - solution['approx1'][0])
+    strong_error['e2'] = np.abs(solution['real'][0] - solution['approx2'][0])
+    strong_error['e3'] = np.abs(solution['real'][0] - solution['approx3'][0])
+    strong_error['e4'] = np.abs(solution['real'][0] - solution['approx4'][0])
+    strong_error['e5'] = np.abs(solution['real'][0] - solution['approx5'][0])
+
+    plot_error = [np.mean(value) for key, value in strong_error.items()]
+    plot_dt = [value for key, value in dt.items() if key not in 'real']
+
+    log_strong_error = np.log10(plot_error)
+    log_dt = np.log10(plot_dt)
+
+    reg_strong = linregress(log_dt, log_strong_error)
+    rate_strong = reg_strong.slope
+    intersection_strong = reg_strong.intercept
+
+    plot_dict = {
+            'beta': beta,
+            'rate': rate_strong,
+            'dt': plot_dt,
+            'error': plot_error,
+            }
+
+    plot = False
+    if plot:
+        fig, ax = plt.subplots()
+        ax.set_title(
+            r'Rate of convergence r = %f for $\beta$=%f' % (rate_strong, beta)
             )
-        )
-solution = dict(zip(keys, solution_tuple))
-#%%
-strong_error = dict.fromkeys(error_keys)
-strong_error['e1'] = np.abs(solution['real'][0] - solution['approx1'][0])
-strong_error['e2'] = np.abs(solution['real'][0] - solution['approx2'][0])
-strong_error['e3'] = np.abs(solution['real'][0] - solution['approx3'][0])
-strong_error['e4'] = np.abs(solution['real'][0] - solution['approx4'][0])
-strong_error['e5'] = np.abs(solution['real'][0] - solution['approx5'][0])
+        ax.plot(plot_dt,
+                plot_error,
+                marker='o',
+                label='Strong error')
+        ax.grid(which='both')
+        ax.set_yscale('log')
+        ax.set_xscale('log')
+        ax.set_xlabel(r'$\log_{10}(\Delta t)$')
+        ax.set_ylabel(r'$\log_{10}(\epsilon)$')
+        ax.legend()
+        plt.show()
 
-plot_error = [np.mean(value) for key, value in strong_error.items()]
-plot_dt = [value for key, value in dt.items() if key not in 'real']
+    #saving = input('Do you want to save to files the plot and its corresponding dictionary? (yes/no): ')
+    saving = False
+    if saving:
+        date_string = time.strftime("%Y-%m-%d-%H-%M")
+        fig.savefig(date_string + '-rate.pdf', dpi=200)
+        with open(date_string + '-dict_plot.pkl', 'wb') as fp:
+            pickle.dump(plot_dict, fp)
+            print('Files saved succesfully')
 
-log_strong_error = np.log10(plot_error)
-log_dt = np.log10(plot_dt)
+    # plt.hist(solution['real'][0][0, :], bins=100, density=True, label="SDE terminal density")
+    # plt.plot(grid_x, solution['real'][1][0, :], label="PDE density t = 0")
+    # plt.plot(grid_x, solution['real'][1][int(points_t/2), :], label="PDE density t = 1/2")
+    # plt.plot(grid_x, solution['real'][1][-1, :], label="PDE density t = 1")
+    # plt.legend()
+    # plt.title(r'Densities real solution with %d time steps for $\beta$=%f' % (time_steps['real'], beta))
+    # plt.show()
+    #
+    # plt.hist(solution['approx1'][0][0, :], bins=100, density=True, label="SDE terminal density")
+    # plt.plot(grid_x, solution['approx1'][1][0, :], label="PDE density t = 0")
+    # plt.plot(grid_x, solution['approx1'][1][int(points_t/2), :], label="PDE density t = 1/2")
+    # plt.plot(grid_x, solution['approx1'][1][-1, :], label="PDE density t = 1")
+    # plt.legend()
+    # plt.title(r'Densities approximation with %d time steps for $\beta$=%f' % (time_steps['approx1'], beta))
+    # plt.show()
+    # 
+    # plt.hist(solution['approx3'][0][0, :], bins=100, density=True, label="SDE terminal density")
+    # plt.plot(grid_x, solution['approx3'][1][0, :], label="PDE density t = 0")
+    # plt.plot(grid_x, solution['approx3'][1][int(points_t/2), :], label="PDE density t = 1/2")
+    # plt.plot(grid_x, solution['approx3'][1][-1, :], label="PDE density t = 1")
+    # plt.legend()
+    # plt.title(r'Densities approximation with %d time steps for $\beta$=%f' % (time_steps['approx3'], beta))
+    # plt.show()
 
-reg_strong = linregress(log_dt, log_strong_error)
-rate_strong = reg_strong.slope
-intersection_strong = reg_strong.intercept
-
-plot_dict = {
-        'beta': beta,
-        'rate': rate_strong,
-        'dt': plot_dt,
-        'error': plot_error,
-        }
-
-plot = True
-if plot:
-    fig, ax = plt.subplots()
-    ax.set_title(
-        r'Rate of convergence r = %f for $\beta$=%f' % (rate_strong, beta)
-        )
-    ax.plot(plot_dt,
-            plot_error,
-            marker='o',
-            label='Strong error')
-    ax.grid(which='both')
-    ax.set_yscale('log')
-    ax.set_xscale('log')
-    ax.set_xlabel(r'$\log_{10}(\Delta t)$')
-    ax.set_ylabel(r'$\log_{10}(\epsilon)$')
-    ax.legend()
-    plt.show()
-
-end_time = time.time()
-running_time = end_time - start_time
-print("Running time is " + str(running_time) + " seconds")
-
-#%%
-#saving = input('Do you want to save to files the plot and its corresponding dictionary? (yes/no): ')
-saving = False
-date_string = time.strftime("%Y-%m-%d-%H-%M")
-if saving:
-    plot_string = date_string + '-rate.pdf'
-    dict_string = date_string + '-dict_plot.pkl'
-    fig.savefig(plot_string, dpi=200)
-    with open(dict_string, 'wb') as fp:
-        pickle.dump(plot_dict, fp)
-        print('Files saved succesfully')
-
-plt.hist(solution['real'][0][0, :], bins=100, density=True, label="SDE terminal density")
-plt.plot(grid_x, solution['real'][1][0, :], label="PDE density t = 0")
-plt.plot(grid_x, solution['real'][1][int(points_t/2), :], label="PDE density t = 1/2")
-plt.plot(grid_x, solution['real'][1][-1, :], label="PDE density t = 1")
-plt.legend()
-plt.title(r'Densities real solution with %d time steps for $\beta$=%f' % (time_steps['real'], beta))
-plt.show()
-
-plt.hist(solution['approx1'][0][0, :], bins=100, density=True, label="SDE terminal density")
-plt.plot(grid_x, solution['approx1'][1][0, :], label="PDE density t = 0")
-plt.plot(grid_x, solution['approx1'][1][int(points_t/2), :], label="PDE density t = 1/2")
-plt.plot(grid_x, solution['approx1'][1][-1, :], label="PDE density t = 1")
-plt.legend()
-plt.title(r'Densities approximation with %d time steps for $\beta$=%f' % (time_steps['approx1'], beta))
-plt.show()
-
-plt.hist(solution['approx3'][0][0, :], bins=100, density=True, label="SDE terminal density")
-plt.plot(grid_x, solution['approx3'][1][0, :], label="PDE density t = 0")
-plt.plot(grid_x, solution['approx3'][1][int(points_t/2), :], label="PDE density t = 1/2")
-plt.plot(grid_x, solution['approx3'][1][-1, :], label="PDE density t = 1")
-plt.legend()
-plt.title(r'Densities approximation with %d time steps for $\beta$=%f' % (time_steps['approx3'], beta))
-plt.show()
+    if loopint == 40:
+        print('rate = ', rate_strong)
+        file = open("rates_mv.txt", "a")
+        file.write(str(rate_strong))
+        file.write("\n")
+        file.close()
